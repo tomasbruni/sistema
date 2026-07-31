1b6df3529de7ab1bfe768f54f6dbaf6cd86fda52
tomas <tomasbruni13@gmail.com>
2026-07-31 15:44:14 -0300

    orden de mas antigua a mas nueva en caja diaria

--- backend-app/app/api/routers/router_caja_diaria.py
+++ backend-app/app/api/routers/router_caja_diaria.py
@@ -702,8 +702,8 @@ def caja_diaria_pdf(
         .where(Venta.fecha_ingreso >= dt_desde)  # type: ignore
         .where(Venta.fecha_ingreso <= dt_hasta)  # type: ignore
         .order_by(
-                Venta.fecha_ingreso.desc(), #type: ignore
-                Venta.venta_id.desc(),  #type: ignore desempate estable para que las páginas no se solapen
+                Venta.fecha_ingreso.asc(), #type: ignore
+                Venta.venta_id.asc(),  #type: ignore desempate estable para que las páginas no se solapen
             )
     ).all()
  
